fcae02ad713b9bc649f3be5448e21c7d20863f27
Ian Harold <ianhunterharold@gmail.com>
2019-07-30 22:08:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,3 @@ def reverse_each_word(word)
   word.each{|word_to_reverse|word_to_reverse.reverse}
   puts word_to_reverse.reverse
 end 
-
-#word = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-#reference code 
-#[1, 2, 3].each { |n| puts "Current number is: #{n}" }
