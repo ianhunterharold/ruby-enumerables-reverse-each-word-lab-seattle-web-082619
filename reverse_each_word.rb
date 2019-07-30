e0bee7c81a221dbb6070b5cf116fbb9aeaf023de
Ian Harold <ianhunterharold@gmail.com>
2019-07-30 22:19:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string)
-  string.each{|word| word.reverse}
+def reverse_each_word(sentence1)
+  sentence1.each{|word| word.reverse}
 end 
 
 #array.each {|item| puts "The current array item is: #{item}"}
