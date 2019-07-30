d354ed280b56c4f0b6140397455411b7433db98a
Ian Harold <ianhunterharold@gmail.com>
2019-07-30 22:33:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-def reverse_each_word(string)
-orginal_array = string.split(" ")
-return_array = []
-original_array.each do|string|
+  
+  
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
 end
-end 
 
 #array.each {|item| puts "The current array item is: #{item}"}
 
